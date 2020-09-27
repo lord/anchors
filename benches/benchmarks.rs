6bb5bb724d0aeebdfafe16b772bedd5934c391f8
Robert Lord <robert@lord.io>
2020-09-27 20:15:23 +0000

    remove smallheight, no longer needed after heap upgrades

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -37,42 +37,6 @@ fn stabilize_linear_nodes_simple(c: &mut Criterion) {
     }
 }
 
-fn stabilize_linear_nodes_smallheight(c: &mut Criterion) {
-    for node_count in &[10, 100] {
-        for observed in &[true, false] {
-            c.bench_with_input(
-                BenchmarkId::new(
-                    "stabilize_linear_nodes_smallheight",
-                    format!(
-                        "{}/{}",
-                        node_count,
-                        if *observed { "observed" } else { "unobserved" }
-                    ),
-                ),
-                &(*node_count, *observed),
-                |b, (node_count, observed)| {
-                    let mut engine = Engine::new_with_max_height(128);
-                    let (first_num, set_first_num) = Var::new(0u64);
-                    let mut node = first_num;
-                    for _ in 0..*node_count {
-                        node = node.map(|val| val + black_box(1));
-                    }
-                    if *observed {
-                        engine.mark_observed(&node);
-                    }
-                    assert_eq!(engine.get(&node), *node_count);
-                    let mut update_number = 0;
-                    b.iter(|| {
-                        update_number += 1;
-                        set_first_num.set(update_number);
-                        assert_eq!(engine.get(&node), update_number + *node_count);
-                    });
-                },
-            );
-        }
-    }
-}
-
 fn stabilize_linear_nodes_cutoff(c: &mut Criterion) {
     for node_count in &[10, 100, 1000] {
         for observed in &[true, false] {
@@ -124,6 +88,6 @@ fn stabilize_linear_nodes_cutoff(c: &mut Criterion) {
 criterion_group! {
     name = benches;
     config = Criterion::default();
-    targets = stabilize_linear_nodes_cutoff, stabilize_linear_nodes_smallheight, stabilize_linear_nodes_simple
+    targets = stabilize_linear_nodes_cutoff, stabilize_linear_nodes_simple
 }
 criterion_main!(benches);
